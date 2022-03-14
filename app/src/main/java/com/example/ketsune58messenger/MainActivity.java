80cca3d8e04a8ae0ddb049099eb7aa1a941039d4
Yakunchev Alexandr <Sasha.yakunchev@yandex.ru>
2022-03-14 18:17:13 +0300

    Added chat translation En->Jp

--- app/src/main/java/com/example/ketsune58messenger/MainActivity.java
+++ app/src/main/java/com/example/ketsune58messenger/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.ketsune58messenger;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,8 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -33,12 +31,12 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("message");
 
-    private static int MAX_MESSAGE_LENGTH = 100;
+    private static final int MAX_MESSAGE_LENGTH = 100;
 
+    ArrayList<String> messagesStorage = new ArrayList<>();
     RecyclerView mRecyclerviewOutput;
     EditText mEditTextInput;
     Button mButtonInput;
-
     FirebaseTranslator englishJapanTranslator;
 
     @Override
@@ -46,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> messagesStorage = new ArrayList<>();
 
         mRecyclerviewOutput = findViewById(R.id.message_output_recyclerview);
         mEditTextInput = findViewById(R.id.message_input_field);
@@ -54,44 +51,40 @@ public class MainActivity extends AppCompatActivity {
 
         DataAdapter dataAdapter = new DataAdapter(this, messagesStorage); //sending array list to adapter
 
-
         mRecyclerviewOutput.setLayoutManager(new LinearLayoutManager(this));
 
         mRecyclerviewOutput.setAdapter(dataAdapter);
 
         FirebaseTranslatorOptions options =
                 new FirebaseTranslatorOptions.Builder()
-                        // below line we are specifying our source language.
                         .setSourceLanguage(FirebaseTranslateLanguage.EN)
-                        // in below line we are displaying our target language.
                         .setTargetLanguage(FirebaseTranslateLanguage.JA)
-                        // after that we are building our options.
                         .build();
-        // below line is to get instance
-        // for firebase natural language.
+
         englishJapanTranslator = FirebaseNaturalLanguage.getInstance().getTranslator(options);
 
-        mButtonInput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String msg = mEditTextInput.getText().toString();
-                if(msg.equals("") || msg.length()>MAX_MESSAGE_LENGTH){
-                    return;
-                }
-
-                //translateLanguage(mEditTextInput.getText().toString());
-                myRef.push().setValue(msg);
-                mRecyclerviewOutput.smoothScrollToPosition(messagesStorage.size());
-                downloadModal(mEditTextInput.getText().toString());
-                mEditTextInput.setText("");
+        mButtonInput.setOnClickListener(view -> {
+            String msg = mEditTextInput.getText().toString();
+            if(msg.equals("") || msg.length()>MAX_MESSAGE_LENGTH){
+                return;
             }
+
+            myRef.push().setValue(msg);
+
+            mRecyclerviewOutput.smoothScrollToPosition(messagesStorage.size());
+
+            //downloadModal(mEditTextInput.getText().toString());
+
+            mEditTextInput.setText("");
         });
 
         myRef.addChildEventListener(new ChildEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { //start each time for every item in database branch
                 String msg = snapshot.getValue(String.class);
-                messagesStorage.add(msg); //filling array list with firebase elements
+                downloadModal(msg);
+                //messagesStorage.add(msg); //filling array list with firebase elements
                 dataAdapter.notifyDataSetChanged();
             }
 
@@ -124,40 +117,25 @@ public class MainActivity extends AppCompatActivity {
         FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder().requireWifi().build();
 
         // below line is use to download our modal.
-        englishJapanTranslator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
+        englishJapanTranslator.downloadModelIfNeeded(conditions).addOnSuccessListener(aVoid -> {
 
-                // this method is called when modal is downloaded successfully.
-                Toast.makeText(MainActivity.this, "Please wait language modal is being downloaded.", Toast.LENGTH_SHORT).show();
+            // this method is called when modal is downloaded successfully.
+            //Toast.makeText(MainActivity.this, "Please wait language modal is being downloaded.", Toast.LENGTH_SHORT).show();
 
-                // calling method to translate our entered text.
-                translateLanguage(input);
+            // calling method to translate our entered text.
+            translateLanguage(input);
 
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "Fail to download modal", Toast.LENGTH_SHORT).show();
-            }
-        });
+        }).addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Fail to download modal", Toast.LENGTH_SHORT).show());
     }
 
     private void translateLanguage(String input) {
-//            Toast.makeText(MainActivity.this, englishJapanTranslator.translate("Hi").toString(), Toast.LENGTH_SHORT).show();
-//            mEditTextInput.setText(englishJapanTranslator.translate(input).toString());
-        englishJapanTranslator.translate(input).addOnSuccessListener(new OnSuccessListener<String>() {
-            @Override
-            public void onSuccess(String s) {
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-                mEditTextInput.setText(s);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                mEditTextInput.setText(e.toString());
-                Toast.makeText(MainActivity.this, "Fail to translate", Toast.LENGTH_SHORT).show();
-            }
+        englishJapanTranslator.translate(input).addOnSuccessListener(s -> {
+            //Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            //mEditTextInput.setText(s);
+            messagesStorage.add(s);
+        }).addOnFailureListener(e -> {
+            mEditTextInput.setText(e.toString());
+            //Toast.makeText(MainActivity.this, "Fail to translate", Toast.LENGTH_SHORT).show();
         });
     }
 }
